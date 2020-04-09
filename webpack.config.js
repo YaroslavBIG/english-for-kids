102bb22c3631f26fed311e1e3dbe9cdfa271a492
YaroslavBIG <yaroslavbig@ya.ru>
2020-04-09 21:02:27 +0300

    feet:config WebPack and eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,69 @@
+/* eslint-disable no-unused-vars */
 const webpack = require('webpack');
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CLIEngine } = require('eslint');
 
 module.exports = (env, options) => {
-    const isProduction = options.mode === 'production';
+  const isProduction = options.mode === 'production';
 
-    const config = {
-        mode: isProduction ? 'production' : 'development',
-        devtool: isProduction ? 'none' : 'source-map',
-        watch: !isProduction,
-        entry: ['./src/index.js', './src/sass/style.scss'],
-        output: {
-            path: path.join(__dirname, '/dist'),
-            filename: 'script.js',
+  const config = {
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? 'none' : 'source-map',
+    watch: !isProduction,
+    entry: ['./src/index.js', './src/sass/style.scss'],
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'script.js',
+    },
+
+    module: {
+      rules: [
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader',
+        }, {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        }, {
+          test: /\.scss$/,
+          use: [
+            'html-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader',
+          ],
+        }, {
+          test: /\.(png|svg|jpe?g|gif)$/,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ],
+        }, {
+          test: /\.html$/,
+          loader: 'html-loader',
         },
+      ],
+    },
 
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: {
-                      loader: 'babel-loader',
-                      options: {
-                        presets: ['@babel/preset-env']
-                      }
-                    }
-                }, {
-                    test: /\.scss$/,
-                    use: [
-                        'html-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' 
-                    ]
-                }, {
-                    test: /\.(png|svg|jpe?g|gif)$/,
-                    use: [
-                        {
-                            loader: 'file-loader',
-                        }
-                    ]
-                }, {
-                    test: /\.html$/,
-                    loader: 'html-loader',
-                },
-            ]
-        },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: 'index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'style.css',
+      }),
+    ],
+  };
 
-        plugins: [
-            new CleanWebpackPlugin(),
-            new HtmlWebpackPlugin({
-                template: 'index.html'
-            }),
-            new MiniCssExtractPlugin({
-                filename: 'style.css'
-            })
-        ]
-    }
-
-    return config;
-}
+  return config;
+};
